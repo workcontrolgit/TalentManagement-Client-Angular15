ed64e034b90df66db58c99a3ab2139ad53451b0b
Fuji <fuji.nguyen@workcontrol.com>
2023-01-29 16:43:04 -0500

    Code clean up for medium publication

--- src/app/features/employee/employee-routing.module.ts
+++ src/app/features/employee/employee-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
-import { AuthorizeComponent } from './authorize.component';
+import { EmployeeComponent } from './employee.component';
 import { AuthGuard } from '@app/core/auth/auth-guard.service';
 
 const routes: Routes = [
   // Module is lazy loaded, see app-routing.module.ts
   {
     path: '',
-    component: AuthorizeComponent,
+    component: EmployeeComponent,
     canActivate: [AuthGuard],
     data: { title: marker('Authorize') },
   },
